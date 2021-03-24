533262d517e9266c221f3f5e8980b1c4565a56df
Rodrigo Álvarez <incognia@gmail.com>
2021-03-23 23:50:42 -0600

    Update commit

--- Arduino/01 Básico/01_LED.ino
+++ Arduino/01 Básico/01_LED.ino
@@ -1,14 +1,41 @@
-int tiempo = 500; // Definimos el tiempo en milisegundos (ms)
+/*
+    LED
 
+    Turns an LED on for one second, then off for one second, repeatedly.
+    
+    Copyright © 2021, Rodrigo Ernesto Alvarez Aguilera <incognia@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Version 0.1 - March 23, 2021
+    Author: Rodrigo Ernesto Alvarez Aguilera
+*/
+
+// Definimos nuestra primera variable (tiempo) de tipo entero (int)
+int tiempo = 1000;                      // El valor está en milisegundos
+
+// La función «setup» se ejecuta una sola vez cuando encendemos la tarjeta
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT); // Configutramos el LED integrado como salida
+    pinMode(LED_BUILTIN, OUTPUT);       // Configutramos el LED integrado como salida
 }
 
+// La función «loop» se ejecuta indefinidamente mientras la tarjeta tenga energía
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH); // Encendemos LED integrado
-    delay(tiempo); // Esperamos la cantidad de milisegundos definidos en la variable «tiempo»
-    digitalWrite(LED_BUILTIN, LOW); // Apagamos el LED integrado
-    delay(tiempo); // Esperamos de nuevo y reiniciamos el ciclo (loop)
+    digitalWrite(LED_BUILTIN, HIGH);    // Encendemos LED integrado (HIGH)
+    delay(tiempo);                      // Esperamos un segundo
+    digitalWrite(LED_BUILTIN, LOW);     // Apagamos LED integrado (LOW)
+    delay(tiempo);                      // Esperamos un segundo
 }
---
+++ Arduino/minimo.ino
@@ -0,0 +1,33 @@
+/*
+    Mímimo
+
+    Código mínimo de ejemplo con licencia GNU.
+    
+    Copyright © 2021, Rodrigo Ernesto Alvarez Aguilera <incognia@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Version 0.1 - January 01, 2021
+    Author: Rodrigo Ernesto Alvarez Aguilera
+*/
+
+void setup() {
+    // coloca tu código de configuración para que se ejecuta una vez:
+
+}
+
+void loop() {
+    // coloca tu código principal para quq se ejecute repetidamente:
+
+}
